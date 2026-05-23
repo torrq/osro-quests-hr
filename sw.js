8dfc1fef0265c081af14c4e3c1c17fd47af75610
torrq <120230227+torrq@users.noreply.github.com>
2026-05-22 22:29:37 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,8 +6,8 @@ self.addEventListener('push', function(event) {
     const options = {
       body: data.body,
       // You can add paths to your actual icons here
-      icon: '/icon.png', 
-      badge: '/badge.png',
+//      icon: '/icon.png', 
+//      badge: '/badge.png',
       vibrate: [100, 50, 100],
       data: {
         dateOfArrival: Date.now(),
